9352316777dda6b55489f3fa4e5b8ad474f4b6b8
syedshahmirsultan <syedshahmirsultan@gmail.com>
2025-06-22 19:06:13 +0500

    AAdded historian_agent

--- src/agenticai/__init__.py
+++ src/agenticai/__init__.py
@@ -1,2 +1,2 @@
 def main() -> None:
-    print("Hello from agenticai!")
+    print("Hello from Shahmir!")
--- src/agenticai/agents.py
+++ src/agenticai/agents.py
@@ -1,1 +1,24 @@
-from agents import Agent,Runner
+import asyncio
+from openai import AsyncOpenAI
+from agents import Agent,Runner,OpenAIChatCompletionsModel
+from dotenv import load_dotenv
+import os
+# Load environment variables from .env file
+load_dotenv()
+
+openrouter_api_key = os.getenv("OPENROUTER_API_KEY")
+BASE_URL = "https://openrouter.ai/api/v1"
+MODEL = "google/gemini-2.0-flash-001"
+
+client = AsyncOpenAI(
+base_url=BASE_URL,
+api_key=openrouter_api_key)
+
+model = OpenAIChatCompletionsModel(openai_client=client, model=MODEL)
+
+
+historian_Agent = Agent(name="Shahmir Agent",instructions="You are an expert Hisrtorian and Philosopher. You are able to answer questions about history and philosophy .The answers should always be precise under 50 words.",model= model)
+
+def main():
+    result = Runner.run_sync(historian_Agent,"When Pakistn got independence? What was the role of Allama Iqbal in Pakistan's independence?")
+    return result.final_output
