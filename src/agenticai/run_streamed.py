e8e6b31b2993d91e5b65da1d6ece96a7295040fd
syedshahmirsultan <syedshahmirsultan@gmail.com>
2025-07-01 19:47:31 +0500

    Updated code

--- src/agenticai/run_streamed.py
+++ src/agenticai/run_streamed.py
@@ -1,6 +1,7 @@
 from agents import OpenAIChatCompletionsModel,Agent,Runner
 from openai import AsyncOpenAI
 from dotenv import load_dotenv
+from openai.types.responses import ResponseTextDeltaEvent
 import asyncio
 import os
 load_dotenv()
@@ -21,9 +22,11 @@ model=model)
 
 async def main():
     result = Runner.run_streamed(agent,"When was the first Programming Language was introduced ?",)
-    async for events in result.stream_events():
-        if (events.type == "raw_response_event" and events.data.type == "response.output_text.delta"):
-            print(events.data.delta)
+    async for event in result.stream_events():
+        # if (events.type == "raw_response_event" and events.data.type == "response.output_text.delta"):
+        #     print(events.data.delta)
+        if event.type == "raw_response_event" and isinstance(event.data,ResponseTextDeltaEvent):
+            print(event.data.delta)
 
 
 def run_main():
